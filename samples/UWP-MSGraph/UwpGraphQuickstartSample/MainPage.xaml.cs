42a83ddc87e2c4a3faf68324b4e16abc222ef057
Shane Weaver <shweaver@microsoft.com>
2021-10-14 16:47:32 -0700

    Added UWP+Graph sample

---
+++ samples/UWP-MSGraph/UwpGraphQuickstartSample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Authentication;
+using CommunityToolkit.Graph.Extensions;
+using Windows.UI.Xaml.Controls;
+
+namespace UwpGraphQuickstartSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ProviderManager.Instance.ProviderStateChanged += OnProviderStateChanged;
+        }
+
+        private async void OnProviderStateChanged(object sender, ProviderStateChangedEventArgs e)
+        {
+            if (e.NewState == ProviderState.SignedIn)
+            {
+                SignedInUserTextBlock.Text = "Signed in as...";
+
+                var graphClient = ProviderManager.Instance.GlobalProvider.GetClient();
+                var me = await graphClient.Me.Request().GetAsync();
+
+                SignedInUserTextBlock.Text = "Signed in as: " + me.DisplayName;
+            }
+            else
+            {
+                SignedInUserTextBlock.Text = "Please sign in.";
+            }
+        }
+    }
+}
