7d3e984aef7eeb3349e3d0ad9245155b44294413
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-10-19 11:50:32 -0700

    Add Windows Community Toolkit Sample

---
+++ samples/UWP-WCT/MyFirstToolkit/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MyFirstToolkit
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<Email> Emails = new ObservableCollection<Email>
+        {
+            new Email { From = "Steve Johnson", Subject = "Lunch Tomorrow", Body = "Are you available for lunch tomorrow? A client would like to discuss a project with you." },
+            new Email { From = "Becky Davidson", Subject = "Kids game", Body = "Don't forget the kids have their soccer game this Friday. We have to supply end of game snacks." },
+            new Email { From = "OneDrive", Subject = "Check out your event recap", Body = "Your new album.\r\nYou uploaded some photos to your OneDrive and automatically created an album for you." },
+            new Email { From = "Twitter", Subject = "Follow randomPerson, APersonYouMightKnow", Body = "Here are some people we think you might like to follow:\r\n.@randomPerson\r\nAPersonYouMightKnow" },
+        };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
--- samples/UWP-WCT/uwp-wct.md
+++ samples/UWP-WCT/uwp-wct.md
@@ -1,4 +1,11 @@
 # Build an awesome UWP application with beautiful Windows Community Toolkit
 
-Link to video: https://aka.ms/win-dev/student/osu/uwp-wct/get-started-video
-Link to blog: https://aka.ms/win-dev/student/osu/uwp-wct/get-started-blog
+This repo shows a simple sample using the 7.1.1 version of the Windows Community Toolkit and the [`ListDetailsView`](https://docs.microsoft.com/windows/communitytoolkit/controls/masterdetailsview) control. Link to accompanying video: https://aka.ms/win-dev/student/osu/uwp-wct/get-started-video
+
+It needs Visual Studio 2019 or later to build with the Universal Windows Platform Development workload. See more details in the base [UWP](../UWP/uwp.md) sample of this repo or from the [getting started docs here](https://docs.microsoft.com/windows/uwp/get-started/your-first-app).
+
+## Resources
+
+- WCT Sample App: https://aka.ms/windowstoolkitapp
+- WCT Docs: https://aka.ms/windowstoolkitdocs
+- WCT GitHub: https://aka.ms/windowstoolkit
